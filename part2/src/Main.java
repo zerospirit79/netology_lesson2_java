8eda264c97601a8f35c233f0e05ba233bb97cbdc
Pavel Shilov <61824830+zerospirit79@users.noreply.github.com>
2021-09-29 17:20:56 +0300

    create two file java
    update homework

---
+++ part2/src/Main.java
@@ -0,0 +1,26 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        // Объявляете переменные для входных данных и
+        // параметров программы: начального счёта,
+        // суммы пополнения и тп
+        int amount = 100;
+        int transaction = 1100;
+        int finalCount;
+        int bonus;
+
+        // Условным оператором проверяете превысила ли
+        // сумма пополнения порог и для этих двух разных
+        // сценариев рассчитываете сумму бонуса и выводите
+        // на экран.
+        if (transaction > 1000) {
+            bonus = transaction/100;
+        } else {
+            System.out.println("Вы пополнили на сумму менее 1000 руб");
+            bonus = 0;
+        }
+        finalCount = transaction + amount + bonus;
+        System.out.println("Сумма на счету = " +finalCount + " рублей");
+    }
+}
